33090c35e50e5a46e59c6ec67b0dd24288e5c533
mariarojo <mariatrojo@gmail.com>
2017-11-27 14:58:00 -0800

    adding Fundamentals folder and files

---
+++ Python/Fundamentals/stringsAndLists.py
@@ -0,0 +1,23 @@
+words = "It's thanksgiving day. It's my birthday, too!"
+
+print words.find('day')
+print words.replace("day", "month")
+
+x = [2,54,-2,7,12,98]
+
+print min(x)
+print max(x)
+
+x = ["hello",2,54,-2,7,12,98,"world"]
+
+print x[0]
+print x[-1]
+newList = [x[0], x[-1]]
+print newList
+
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+
+x.sort()
+firstHalf = firstHalf = x[0:len(x)/2]
+x[0:len(x)/2] = [firstHalf]
+print x
