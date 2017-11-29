c6f4fffaf32d263692e54bf35e30934401429167
mariarojo <mariatrojo@gmail.com>
2017-11-28 19:16:07 -0800

    adding tuples.py, edited names.py

---
+++ Python/Fundamentals/names.py
@@ -0,0 +1,36 @@
+"""def players():
+    students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+    for value in students:
+        print value['first_name'],value['last_name']
+
+players()"""
+
+
+def basketball():
+
+    users = {
+    'Students': [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ],
+    'Instructors': [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+    ]
+    }
+
+    for data in users:
+        print data
+        for value in users.itervalues():
+            for value in 'Students':
+                print value['first_name'],value['last_name']
+
+basketball()
